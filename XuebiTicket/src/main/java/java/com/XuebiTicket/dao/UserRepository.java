02ab018a9be5f960af898d6bdcb38d5ba8df1f1b
william <williamjwking@gmail.com>
2016-05-30 23:12:45 +0800

    add dao

---
+++ XuebiTicket/src/main/java/java/com/XuebiTicket/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.XuebiTicket.dao.UserRepository.java
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.XuebiTicket.model.User;
+
+}
